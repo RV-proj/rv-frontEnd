0b1422100f207095003863421576641d0e03bb93
Radwan Fahim <88280947+radwanfahim@users.noreply.github.com>
2025-11-01 13:54:18 +0600

    book flex button hover:bg-cyan-300 added

--- app/_ui/BookFlexButton.js
+++ app/_ui/BookFlexButton.js
@@ -7,7 +7,7 @@ export default function BookFlexButton({ color = "default" }) {
   const colorClasses =
     color === "white"
       ? "bg-white text-slate-900 hover:bg-white/90"
-      : "bg-cyan-400 text-slate-900 hover:bg-white/90";
+      : "bg-cyan-400 text-slate-900 hover:bg-cyan-300 ";
 
   return (
     <button className={`${baseClasses} ${colorClasses} cursor-pointer`}>
