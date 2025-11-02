477fdbf441bdbf556e3f374410d1a3a0be545bc8
Radwan Fahim <88280947+radwanfahim@users.noreply.github.com>
2025-11-02 20:52:33 +0600

    commit

--- app/page.js
+++ app/page.js
@@ -1,18 +1,18 @@
-import Hero from "@/_components/landing/Hero";
-import Pricing from "@/_components/landing/Pricing";
-import Why from "./_components/landing/Why";
-import OurSecretSauce from "./_components/landing/OurSecretSauce";
-import SampleFloorplans from "./_components/landing/SampleFloorplans";
-import Divider from "./_ui/Divider";
-import HappyGuests from "./_components/landing/HappyGuests";
-import ForOwners from "./_components/landing/ForOwners";
-import Faq from "./_components/landing/Faq";
-import Ready from "./_components/landing/Ready";
+import Hero from "@/_components/Landing/Hero";
+import Pricing from "@/_components/Landing/Pricing";
+import Why from "@/_components/Landing/Why";
+import OurSecretSauce from "@/_components/Landing/OurSecretSauce";
+import SampleFloorplans from "@/_components/Landing/SampleFloorplans";
+import Divider from "@/_ui/Divider";
+import HappyGuests from "@/_components/Landing/HappyGuests";
+import ForOwners from "@/_components/Landing/ForOwners";
+import Faq from "@/_components/Landing/Faq";
+import Ready from "@/_components/Landing/Ready";
+import HowItWorks from "@/_components/Landing/HowItWorks";
+import RVSelector from "@/_components/Landing/RVSelector";
+import Included from "@/_components/Landing/Included";
+import VisualStyleByLevel from "./_components/Landing/VisualStyleByLevel";
 import Footer from "./_components/Footer/Footer";
-import HowItWorks from "./_components/landing/HowItWorks";
-import RVSelector from "./_components/landing/RVSelector";
-import Included from "./_components/landing/Included";
-import VisualStyleByLevel from "./_components/landing/VisualStyleByLevel";
 
 export default function Page() {
   return (
