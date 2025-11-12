8c2098329cef86f85289dbc2c46081c0c72ec2aa
Root <abdullahalmasrur8@gmail.com>
2025-11-13 02:14:09 +0600

    order fetch from api, spinnin ui added

--- app/adminpanel/order/all-order/page.js
+++ app/adminpanel/order/all-order/page.js
@@ -1,3 +1,23 @@
+"use client";
+
+import { getOrders } from "@/_lib/api/getOrders";
+import { SpinnerMini } from "@/_ui/Spinner";
+import Table from "@/_ui/Table";
+import { useQuery } from "@tanstack/react-query";
+
 export default function Page() {
-  return <section id="all-order">All order table</section>;
+  const { data, isPending } = useQuery({
+    queryKey: ["orders"],
+    queryFn: getOrders,
+  });
+
+  console.log(data);
+
+  return (
+    <section id="all-order">
+      {/* <p>All order table</p> */}
+
+      {isPending ? <SpinnerMini size="md" /> : <Table data={data} />}
+    </section>
+  );
 }
