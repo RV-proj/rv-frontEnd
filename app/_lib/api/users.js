d500107ba6ded4da61445c92e4e9b90558778bef
Abdullah Al Masrur <abdullahalmasrur8@gmail.com>
2025-11-25 20:27:28 +0600

    Update users.js

--- app/_lib/api/users.js
+++ app/_lib/api/users.js
@@ -1,13 +1,13 @@
 import { auth } from "../authSession/auth";
 
 export async function getUser(email) {
-  const res = await fetch(`http://localhost:5000/user/email/${email}`);
+  const res = await fetch(`https://rv-back-end.vercel.app/user/email/${email}`);
   return await res.json();
 }
 
 export async function getSessionUser() {
   const session = await auth();
-  const res = await fetch(`http://localhost:5000/user/${session.user.userId}`);
+  const res = await fetch(`https://rv-back-end.vercel.app/user/${session.user.userId}`);
 
   return await res.json();
 }
