a43cf81d641083c4be4c2090d87a4b87e57404fb
Root <abdullahalmasrur8@gmail.com>
2025-12-03 01:32:43 +0600

    image fixed

--- app/_lists/prices.js
+++ app/_lists/prices.js
@@ -2,49 +2,74 @@ export const prices = {
   M_Basic: {
     flexPrice: 107,
     marketPrice: 115,
+    image:
+      "https://drive.google.com/thumbnail?id=10Pyxj2KYAnel7-B98uy2XHopOFDng5n8",
   },
   M_Standard: {
     flexPrice: 107,
     marketPrice: 115,
+    image:
+      "https://drive.google.com/thumbnail?id=1RD9RukmWtaxMkLSKdRtUB5H-lnaKyupG",
   },
   M_Premium: {
     flexPrice: 126,
     marketPrice: 135,
+    image:
+      "https://drive.google.com/thumbnail?id=1e4cdnix9YTzUpQ_6fm21G2G3FvvwkVIG",
   },
+
   L_Basic: {
     flexPrice: 179,
     marketPrice: 195,
+    image:
+      "https://drive.google.com/thumbnail?id=10Pyxj2KYAnel7-B98uy2XHopOFDng5n8",
   },
   L_Standard: {
     flexPrice: 207,
     marketPrice: 225,
+    image:
+      "https://drive.google.com/thumbnail?id=1RD9RukmWtaxMkLSKdRtUB5H-lnaKyupG",
   },
   L_Premium: {
     flexPrice: 216,
     marketPrice: 235,
+    image:
+      "https://drive.google.com/thumbnail?id=1e4cdnix9YTzUpQ_6fm21G2G3FvvwkVIG",
   },
   XL_Basic: {
     flexPrice: 107,
     marketPrice: 115,
+    image:
+      "https://drive.google.com/thumbnail?id=10Pyxj2KYAnel7-B98uy2XHopOFDng5n8",
   },
   XL_Standard: {
     flexPrice: 126,
     marketPrice: 135,
+    image:
+      "https://drive.google.com/thumbnail?id=1H2OxOQhn9UeU1WQaA5A-qfIjPzrEaPV6",
   },
   XL_Premium: {
     flexPrice: 175,
     marketPrice: 185,
+    image:
+      "https://drive.google.com/thumbnail?id=1WqGqoVDKkBHn4j3Qr6D-wEcx9DW-Xhko",
   },
   XXL_Basic: {
     flexPrice: 198,
     marketPrice: 215,
+    image:
+      "https://drive.google.com/thumbnail?id=1dghKv5Pl8loXQ6f7n2fU1zwLGEZlAiFp",
   },
   XXL_Standard: {
     flexPrice: 235,
     marketPrice: 255,
+    image:
+      "https://drive.google.com/thumbnail?id=16BkMtvyXCuNhjUYUosV3xbEi5ECMfivv",
   },
   XXL_Premium: {
     flexPrice: 244,
     marketPrice: 265,
+    image:
+      "https://drive.google.com/thumbnail?id=1PehsfH-lZP98qfRufa_J7rl1DtBwE949",
   },
 };
