9a32895d4b528e55130ac415704e218848c4c6c6
Abdullah Al Masrur <abdullahalmasrur8@gmail.com>
2026-01-27 15:00:01 +0600

    Update API endpoints to use production URL

--- app/_lib/api/users.js
+++ app/_lib/api/users.js
@@ -4,7 +4,7 @@ import {
 } from "../authSession/authSession";
 
 export async function getUser(email) {
-  const res = await fetch(`http://localhost:5000/user/email/${email}`);
+  const res = await fetch(`https://rv-front-end-one.vercel.app/user/email/${email}`);
   return await res.json();
 }
 
@@ -12,7 +12,7 @@ export async function getSessionUser() {
   const session = await getAuthenticatedSession();
 
   const data = await fetchWithAuth(
-    `http://localhost:5000/user/${session.user.userId}`,
+    `https://rv-front-end-one.vercel.app/user/${session.user.userId}`,
     session.user.accessToken
   );
 
